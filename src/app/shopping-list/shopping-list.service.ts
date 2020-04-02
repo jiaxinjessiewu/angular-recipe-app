69cd9675c262fcfa35e3fb6c6849fa92d614a008
JessieWu <49457922+jiaxinjessiewu@users.noreply.github.com>
2020-04-01 22:07:07 -0400

    Use Observable

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from "../shared/ingredients.model";
 import { EventEmitter } from "@angular/core";
-
+import { Subject } from 'rxjs';
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apples',5),
@@ -14,10 +14,10 @@ export class ShoppingListService {
     }
     addIngredientAdded(newIngredient : Ingredient) {
         this.ingredients.push(newIngredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
     }
     addIngredients(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients);
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
     }
 }
